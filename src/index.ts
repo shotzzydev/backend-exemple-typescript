8f3ca7b2256f677ccdfd9bc90afa8761f730186d
Austin <luanventuradev@gmail.com>
2025-09-13 17:00:15 -0300

    add static in console.log index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
 import app from './app';
+import { consola } from 'consola'
+import ck from 'chalk';
 
-app.listen(3000, () => {
-    console.log("Servido ouvido na porta 3000")
-})
+export const serverInfo = {
+    port: Number(process.env.PORT || 3000)
+}
+
+app.listen({ port: serverInfo.port }, () => {
+    consola.success(ck.green(`${ck.blue.underline("Backend")} is running on port`))
+    consola.log(ck.magenta(`➝ ${ck.underline("Server initialized. Ready to handle requests!")}`))
+    consola.log(ck.green(`⤿ ${ck.underline("Access it at")} http://127.0.0.1:`))
+});
